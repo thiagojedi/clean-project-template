589283f34731b784ad02d84dfff5c2cbf383a1b3
Thiago Jedi <thiagoa7@gmail.com>
2020-12-25 20:29:21 -0300

    Explaning jest config

--- jest.config.js
+++ jest.config.js
@@ -2,11 +2,13 @@ module.exports = {
   "preset": "ts-jest/presets/js-with-ts",
   "testEnvironment": "jsdom",
   "moduleNameMapper": {
+    // Using this the css modules are not rendered to snapshots
     "\\.(sc|sa|le|c)ss$": "<rootDir>/src/__mocks__/style.mock.js"
   },
   "globals": {
     "ts-jest": {
+      // So the ts-jest can render the JSX
       "tsconfig": "tsconfig.jest.json"
     }
   }
-}
+};
